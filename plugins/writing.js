7a91907b41b201056280c5da477431598d191895
HarmanPC <harmansinghgwalior4@gmail.com>
2018-02-16 16:40:32 +0530

    Myth of the day for Canalave Library.
    I have made background purple as Felis's choice, Format: ``;motd [Myth name], [Description of Myth], [Image link of Myth]``

--- plugins/writing.js
+++ plugins/writing.js
@@ -755,6 +755,82 @@ let commands = {
 		Storage.exportDatabase('writing');
 		this.say(text + "The Word of the Day has been set to '" + targets[0] + "'!");
 	},
+	// Returns the myth of the day for mythology room
+	'mythoftheday': 'motd',
+	motd: function (target, room, user) {
+		let text = room instanceof Users.User || user.hasRank(room, '+') ? '' : '/pm ' + user.name + ', ';
+		if (!target) {
+			if (!database.motd) return this.say(text + "No Myth of the Day has been set.");
+			let tem = new Date(database.motd.time).toLocaleString('en-US', {hour: 'numeric', minute:'numeric', day:'2-digit', month:'long', hour12: true, timeZoneName: 'short'});
+			let box = `<div style="background-image: url(${database.motd.image}); background-size: contain; background-position: center; background-color: black; height: 400px; background-repeat: no-repeat"></div>`;
+			let boxpm = '<div style="background:url(https://i.imgur.com/loMDPK2.jpg) center;margin:-2px -4px;box-shadow:inset 0 0 50px rgba(0,0,0,0.15)"> <div style="font-family:serif;max-width:500px;margin:auto;padding:15px;text-align:justify;"> <span style="display:block;font-family:serif;font-size:14pt;font-style:oblique;background:#6688AA;padding:5px 0;text-align:center;border-radius:2px;color:rgba(255,255,255,1);margin-bottom:2px;">Myth of the Day</span> <span style="font-size:20pt;display:block;">' + database.motd.image + '</span> <span style="font-family:sans-serif;font-size:11pt;display:block;color:rgba(0,0,0,0.7);letter-spacing:2px;">' + database.motd.desc + '</strong></span><span style="font-size:10pt;font-family:sans-serif;margin-top:10px;display:block;color:rgba(0,0,0,0.8)"><strong style="font-family:serif;margin-right:10px;color:rgba(0,0,0,0.5)">1.</strong>' + database.motd.desc + '</span></div></div>';
+			let realBox = `<div style="background:darkorchid;margin:-2px -4px;box-shadow:inset 0 0 50px rgba(0,0,0,0.15)"> <div style="font-family:serif;max-width:500px;margin:auto;padding:15px;text-align:justify;"><span style="display:block;font-family:serif;font-size:18pt;font-style:oblique;background:#6688AA;padding:5px 0;text-align:center;border-radius:2px;color:rgba(255,255,255,1);margin-bottom:2px;"><i class="fa fa-fire" aria-hidden="true"></i> Myth of the Day <i class="fa fa-fire" aria-hidden="true"></i></span> <center>${box}<br><span style="font-size:30pt;display:block;">${database.motd.myth} </span><p style="font-size:19px">Info: ${database.motd.desc}</p></center><span style="font-family:sans-serif;font-size:12pt;display:block;color:rgba(0,0,0,0.7);letter-spacing:2px;"></span><span style="font-size:10pt;font-family:sans-serif;margin-top:10px;display:block;color:rgba(0,0,0,0.8)"><strong style="font-family:serif;margin-right:10px;color:rgba(0,0,0,0.5)"></strong></span><div style="width:100%;padding:2px 0;border:1px solid #6688AA;display:block;font-family:sans-serif;font-size:9.5pt;text-align:center;margin-top:15px;border-radius:2px;"><span><i class="fa fa-refresh" aria-hidden="true"></i> Set by ${database.motd.user} ${tem}</span> </div></div></div>`;
+			if (!(room instanceof Users.User) && user.hasRank(room, '+')) {
+				return this.sayHtml(realBox);
+			} else {
+				// The below is a hacky way to get pminfobox to work within PM. It defaults to Writing since AxeBot/The Scribe is always * in that room. For personal bots, this should be changed to any room that you can guarentee the bot has at least * permissions.
+				if (!(room instanceof Users.User) && Users.self.rooms.get(room) === '*') {
+					return this.pmHtml(user, boxpm);
+				} else {
+					return this.say(text + "Today's Myth of the Day is **" + database.motd.myth + "**: " + database.motd.image + " " + database.motd.desc);
+				}
+			}
+		}
+		if (Tools.toId(target) === 'check' || Tools.toId(target) === 'time') {
+			if (!database.motd) return this.say(text + "There is no Myth of the Day to check!");
+			return this.say(text + "The Myth of the Day was last updated to **" + database.motd.myth + "** " + Tools.toDurationString(Date.now() - database.motd.time) + " ago by " + database.motd.user);
+		}
+		let targets = target.split(',');
+		let typo = false;
+		if (targets[0] === "typo") {
+			if (!database.motd) return this.say(text + "There is no Myth of the Day to correct!");
+			if ((room instanceof Users.User || !user.hasRank(room, '%')) && user.name !== database.motd.user) return this.say(text + "Sorry, you must be the original user or driver and above to make typo corrections.");
+			typo = true;
+			targets.shift();
+		}
+		if (database.motd) {
+			if (!typo && Date.now() - database.motd.time < 61200000) return this.say(text + "Sorry, but at least 17 hours must have passed since the MOTD was last set in order to set it again!");
+		}
+		let hasPerms = false;
+		if (database.scribeShop) {
+			if (typo || (!(room instanceof Users.User) && user.hasRank(room, '+'))) {
+				hasPerms = true;
+			} else {
+				for (let i = 0; i < database.scribeShop.length; i++) {
+					if (database.scribeShop[i].account === user.id) {
+						if (database.scribeShop[i].motd !== 0) {
+							database.scribeShop[i].motd -= 1;
+							hasPerms = true;
+							this.say("Redeeming your Poetic License... Uses remaining: " + database.scribeShop[i].motd + "!");
+						}
+					}
+				}
+			}
+		} else if (!(room instanceof Users.User) && user.hasRank(room, '+')) {
+			hasPerms = true;
+		}
+		if (!hasPerms) return this.say(text + 'You must be at least Voice or higher to set the Myth of the Day.');
+		if (targets.length < 3) return this.say(text + "Invalid arguments specified. The format is: __myth__, __description__, __image link__.");
+		let motd = {
+			myth: targets[0],
+			desc: targets[1],
+			image: targets[2],
+		};
+		if (!typo) {
+			motd.time = Date.now();
+			motd.user = user.name;
+		} else {
+			motd.time = database.motd.time;
+			motd.user = database.motd.user;
+		}
+		if (!database.motdHistory) {
+			database.motdHistory = [];
+		}
+		database.motd = motd;
+		database.motdHistory.push(motd);
+		Storage.exportDatabase('writing');
+		this.say(text + "The Myth of the Day has been set to '" + targets[0] + "'!");
+	},
 	//Returns the link to the Writing Room's website.
 	site: function (target, room, user) {
 		let text = room instanceof Users.User || user.hasRank(room, '+') ? '' : '/pm ' + user.name + ', ';
